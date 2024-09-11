f186bcd395748eb2e2bc5731927204c50f0c12d4
Justin <j.pietzsch@bbw-fi.de>
2024-09-11 16:01:20 +0200

    Edited Style of foods.js, still needs some fixing, but it's a starting point

--- infotafel/src/app/components/foods.js
+++ infotafel/src/app/components/foods.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function Foods() {
@@ -11,7 +11,7 @@ function Foods() {
     const fetchData = async () => {
       try {
         const response = await axios.get("http://localhost:8000/cache/food");
-        console.log("Fetched Data: ", response.data); // Log the data here
+        console.log("Fetched Data: ", response.data);
         setMealPlan(Array.isArray(response.data.parsedFooddata) ? response.data.parsedFooddata : []);
         setLoading(false);
       } catch (error) {
@@ -23,26 +23,31 @@ function Foods() {
     fetchData();
   }, []);
 
-  let mealIndex = 0;
-
   return (
     <div className="bg-site-background w-screen mx-auto px-4 text-white flex-grow min-w-screen flex justify-center">
-      <div className="flex flex-row items-center w-fit h-auto">
+      <div className="flex flex-row items-start w-fit h-auto space-x-4">
         {loading ? (
           <div className="text-2xl">Loading...</div>
         ) : Array.isArray(mealPlan) && mealPlan.length > 0 ? (
           mealPlan.map((day) => (
             <div
-              className="flex flex-col bg-gray-700 p-4 m-4 rounded-lg"
+              className="flex flex-col bg-gray-700 p-4 m-4 rounded-lg w-64 flex-grow"
               key={day.date}
             >
               <h1 className="text-2xl font-bold">{day.date}</h1>
-              <div className="flex flex-col">
-                {/* Check if menus object and menuName array exist before mapping */}
+              <div className="flex flex-col space-y-2 h-1000">
                 {day.meals && day.meals.menus && Array.isArray(day.meals.menus.menuName) && day.meals.menus.menuName.length > 0 ? (
                   day.meals.menus.menuName.map((meal, mealIndex) => (
-                    <div className="flex flex-row" key={mealIndex}>
+                    <div
+                      className="bg-gray-600 p-2 rounded-md h-100 flex flex-col justify-between"
+                      key={mealIndex}
+                    >
                       <p className="text-xl">{meal}</p>
+                      {day.meals.menus.alergenes[mealIndex] ? (
+                        <p className="text-sm text-gray-400">
+                          Allergene: {day.meals.menus.alergenes[mealIndex]}
+                        </p>
+                      ) : null}
                     </div>
                   ))
                 ) : (
