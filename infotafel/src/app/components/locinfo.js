c85157862f54f74d4fc2deed3e4692ec728f56ca
Prof-Dr-C4-254 <e.nessler@bbw-fi.de>
2024-09-11 12:02:33 +0200

    Create locinfo.js

---
+++ infotafel/src/app/components/locinfo.js
@@ -0,0 +1,51 @@
+export default function LocInfo(){
+    return(
+        <div className="mx-10 ml-52 mt-16 w-full flex-1 flex h-1000  ">
+            <div className=" w-7/12 flex flex-col ">
+                <div className="w-5/12 justify-end">
+                    <div className="mt-20 text-6xl font-semibold ">WG:</div>
+                        <div className="mt-8 font-thin text-4xl">Vallah-Billah-Straße.187</div>
+                    <div className="mt-36 text-6xl font-semibold ">Betreuer:</div>
+                        <div className="mt-8 font-thin text-4xl">Herr Typ-da</div>
+                    <div className="mt-36 text-6xl font-semibold ">Tel.Nummer:</div>
+                        <div className="mt-8 font-thin text-4xl">06516516511</div>
+                </div>
+            </div>
+
+
+            <div className="w-7/12 flex flex-col ">
+                <div className="w-5/12 justify-end">
+                    <div className="mt-20 text-6xl font-semibold ">WG:</div>
+                        <div className="mt-8 font-thin text-4xl">Vallah-Billah-Straße.187</div>
+                    <div className="mt-36 text-6xl font-semibold ">Betreuer:</div>
+                        <div className="mt-8 font-thin text-4xl">Herr Typ-da</div>
+                    <div className="mt-36 text-6xl font-semibold ">Tel.Nummer:</div>
+                        <div className="mt-8 font-thin text-4xl">06516516511</div>
+                </div>
+            </div>
+
+            <div className="w-7/12 flex flex-col ">
+                <div className="w-5/12 justify-end">
+                    <div className="mt-20 text-6xl font-semibold ">WG:</div>
+                        <div className="mt-8 font-thin text-4xl">Vallah-Billah-Straße.187</div>
+                    <div className="mt-36 text-6xl font-semibold ">Betreuer:</div>
+                        <div className="mt-8 font-thin text-4xl">Herr Typ-da</div>
+                    <div className="mt-36 text-6xl font-semibold ">Tel.Nummer:</div>
+                        <div className="mt-8 font-thin text-4xl">06516516511</div>
+                </div>
+            </div>
+
+
+            <div className="w-7/12 flex flex-col ">
+                <div className="w-5/12 justify-end">
+                    <div className="mt-20 text-6xl font-semibold ">WG:</div>
+                        <div className="mt-8 font-thin text-4xl">Vallah-Billah-Straße.187</div>
+                    <div className="mt-36 text-6xl font-semibold ">Betreuer:</div>
+                        <div className="mt-8 font-thin text-4xl">Herr Typ-da</div>
+                    <div className="mt-36 text-6xl font-semibold ">Tel.Nummer:</div>
+                        <div className="mt-8 font-thin text-4xl">06516516511</div>
+                </div>
+            </div>
+        </div>
+    );
+}
