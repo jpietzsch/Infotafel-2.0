ca9f59d9ac9d5c0fa1bb58cd5d066811fbed0fe7
Justin <j.pietzsch@bbw-fi.de>
2024-09-12 09:56:12 +0200

    Restore Benni cache

--- infotafel/server/cache.js
+++ infotafel/server/cache.js
@@ -11,134 +11,67 @@ const PORT = 8000;
 const latitude = 50.84310553905255;
 const longitude = 12.875960703035425;
 
+
 app.use(cors());
 
-const CACHE_FILE_BUSPLAN = path.join(__dirname, "busplan_cache.json");
-const CACHE_FILE_MEALPLAN = path.join(__dirname, "mealplan_cache.json");
-const CACHE_FILE_WEATHER = path.join(__dirname, "weather_cache.json");
+const CACHE_FILE = path.join(__dirname, "cache.json");
+
+// full api link
+// https://api.open-meteo.com/v1/dwd-icon?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,cloud_cover,apparent_temperature,rain,weather_code,wind_speed_10m,wind_direction_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,rain_sum,wind_speed_10m_max&&hourly=temperature_2m,rain,weather_code&forecast_days=3&timeformat=unixtime&timezone=Europe%2FBerlin
+// full api link
+// https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,apparent_temperature,rain,weather_code,cloud_cover,wind_speed_10m,wind_direction_10m&hourly=temperature_2m,rain,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timeformat=unixtime
+
+
+//#region [rgba(90,155,243,0.0)] WEATHER DATA
+// Function to fetch current weather data from the online API
 
 const fetchWeatherData = async () => {
   try {
     const response = await axios.get(
-      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,apparent_temperature,rain,weather_code,cloud_cover,wind_speed_10m,wind_direction_10m&hourly=temperature_2m,rain,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timeformat=unixtime`
-    );
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,apparent_temperature,rain,weather_code,cloud_cover,wind_speed_10m,wind_direction_10m&hourly=temperature_2m,rain,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timeformat=unixtime`,
+    )
     return response.data;
   } catch (error) {
     console.error("Error fetching weather data:", error);
     throw error;
   }
-};
+}
+
+
 
 const isTimestampExpired = (timestamp, expirationTime) => {
+  //Time in Minutes
   return Date.now() - timestamp >= expirationTime * 60 * 1000;
-};
+}
 
 app.get("/cache/weather", async (req, res) => {
   try {
-    if (fs.existsSync(CACHE_FILE_WEATHER)) {
-      const cachedData = JSON.parse(fs.readFileSync(CACHE_FILE_WEATHER, "utf8"));
-      if (!isTimestampExpired(cachedData.timestamp, 5)) {
-        res.json(cachedData.data);
-        return;
+    let cache = {};
+    if (fs.existsSync(CACHE_FILE)) {
+      cache = JSON.parse(fs.readFileSync(CACHE_FILE));
+      if (cache.weather && cache.weather.timestamp && !isTimestampExpired(cache.weather.timestamp, 5)) {
+        console.log("return cached weather data")
+        return res.json(cache.weather.data);
+      } else {
+        console.log("cache expired")
       }
     }
     let data = await fetchWeatherData();
-    let timestamp = Date.now();
-    const cacheData = { data, timestamp };
-    fs.writeFileSync(CACHE_FILE_WEATHER, JSON.stringify(cacheData));
+    cache.weather = {
+        timestamp: Date.now(),
+        data
+      }
+    fs.writeFileSync(CACHE_FILE, JSON.stringify(cache, null, 2));
+    console.log("trigger weather cache");
+
     res.json(data);
-  } catch (error) {
+
+  } catch(error) {
     res.status(500).send("Failed to retrieve weather data");
   }
-});
+})
+//#endregion
 
-async function fetchHTML(url) {
-  try {
-    const response = await axios.get(url);
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching HTML:", error);
-    throw error;
-  }
-}
-
-function parseHTML(html) {
-  const $ = cheerio.load(html);
-  const data = [];
-  $("div.std3_departure-line").each((index, element) => {
-    const lineNr = $(element)
-      .find(".std3_dm-mot-info a")
-      .text()
-      .trim()
-      .replace(/[^0-9]/g, "")
-      .substring(2, 0);
-    const desc = $(element)
-      .find(".std3_dm-mot-info")
-      .text()
-      .trim()
-      .replace("Stadtbus", "")
-      .replace(/\d+/g, "");
-    const time = $(element).find(".std3_dm-time:first").text().trim();
-    const realtime = $(element).find(".std3_realtime-column").text().trim();
-    const existingEntryIndex = data.findIndex(
-      (entry) => entry.lineNr === lineNr && entry.desc === desc
-    );
-
-    if (existingEntryIndex !== -1) {
-      if (!realtime) {
-        data[existingEntryIndex].times.push({ time });
-      } else {
-        data[existingEntryIndex].times.push({ time, realtime });
-      }
-    } else {
-      if (!realtime) {
-        data.push({ lineNr, desc, times: [{ time }] });
-      } else {
-        data.push({ lineNr, desc, times: [{ time, realtime }] });
-      }
-    }
-    console.log(data);
-  });
-
-  return data;
-}
-
-function formatBusData(parsedData) {
-  const busData = {};
-  parsedData.forEach((entry) => {
-    const { lineNr, desc, times } = entry;
-    const departureTimes = times.map((timeObj) => timeObj.time);
-    const realTimes = times.map((timeObj) => timeObj.realtime);
-    busData[lineNr.trim()] = { desc: desc.trim(), departureTimes, realTimes };
-  });
-  return busData;
-}
-
-app.get("/cache/busplan", async (req, res) => {
-  const baseUrl = "https://efa.vvo-online.de/VMSSL3/XSLT_DM_REQUEST";
-  const currentDate = new Date();
-  const url = `${baseUrl}?language=de&std3_suggestMacro=std3_suggest&std3_commonMacro=dm&itdDateTimeDepArr=dep&name_dm=Chemnitz%2C+Flemmingstr&type_dm=any&itdDateDayMonthYear=${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}&itdTime=${currentDate.getHours()}%3A${currentDate.getMinutes()}`;
-
-  try {
-    if (fs.existsSync(CACHE_FILE_BUSPLAN)) {
-      const cachedData = JSON.parse(fs.readFileSync(CACHE_FILE_BUSPLAN, "utf8"));
-      if (!isTimestampExpired(cachedData.timestamp, 1)) {
-        res.json(cachedData);
-        return;
-      }
-    }
-    const html = await fetchHTML(url);
-    const timestamp = Date.now();
-    const parsedData = parseHTML(html);
-    const busData = formatBusData(parsedData);
-    const cacheData = { timestamp, busData };
-    fs.writeFileSync(CACHE_FILE_BUSPLAN, JSON.stringify(cacheData));
-    res.json(cacheData);
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
 
 async function parseFoodHTML(html) {
   const $ = cheerio.load(html);
@@ -158,6 +91,7 @@ async function parseFoodHTML(html) {
           if (j === 2) {
             meals.soup.soupName.push(menuParts[0].trim());
             meals.soup.alergens.push(menuParts[1].trim());
+
           } else {
             meals.menus.menuName.push(menuParts[0].trim());
             meals.menus.alergenes.push(menuParts[1].trim());
@@ -172,24 +106,38 @@ async function parseFoodHTML(html) {
   return data;
 }
 
+
 app.get("/cache/food", async (req, res) => {
   try {
-    if (fs.existsSync(CACHE_FILE_MEALPLAN)) {
-      const cachedData = JSON.parse(fs.readFileSync(CACHE_FILE_MEALPLAN, "utf8"));
-      res.json(cachedData.parsedFooddata); // Return only the parsed data
-      return;
+    let cache = {};
+
+    if (fs.existsSync(CACHE_FILE)) {
+      cache = JSON.parse(fs.readFileSync(CACHE_FILE));
+      if (cache.food && cache.food.timestamp && !isTimestampExpired(cache.food.timestamp, 5)) {
+        console.log("return cached food data");
+        return res.json(cache.food.data);
+      } else {
+        console.log("cache expired");
+      }
     }
 
     try {
       console.log("Fetching speiseplan data...");
-      const response = await axios.get(`https://www.kantine-chemnitz.de/speiseplan.html`);
+      const response = await axios.get(
+        `https://www.kantine-chemnitz.de/speiseplan.html`,
+      );
       const parsedFooddata = await parseFoodHTML(response.data);
 
       const timestamp = Date.now();
-      const cacheData = { parsedFooddata, timestamp };
-      fs.writeFileSync(CACHE_FILE_MEALPLAN, JSON.stringify(cacheData));
 
-      res.json(parsedFooddata); // Send parsedFooddata
+      cache.food = {
+        timestamp: Date.now(),
+        parsedFooddata
+      }
+
+      fs.writeFileSync(CACHE_FILE, JSON.stringify(cache, null, 2));
+
+      res.json(parsedFooddata);
     } catch (error) {
       console.error("Error fetching speiseplan data:", error);
       throw error;
@@ -200,6 +148,9 @@ app.get("/cache/food", async (req, res) => {
   }
 });
 
+
+
+
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
